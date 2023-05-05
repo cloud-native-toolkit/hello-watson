a56dace231cdf257616f18099320e80616eff173
Sean Sundberg <seansund@us.ibm.com>
2023-05-05 12:46:05 -0400

    Add security info to openapi spec
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/dex/hellowatson/config/OpenApiConfiguration.java
+++ src/main/java/com/dex/hellowatson/config/OpenApiConfiguration.java
@@ -2,12 +2,14 @@ package com.dex.hellowatson.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -28,7 +30,9 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI openApiConfig() {
         return new OpenAPI()
-                .info(buildOpenApiInfo());
+                .info(buildOpenApiInfo())
+                // TODO fix
+                .security(List.of(new SecurityRequirement().addList("basic_auth")));
     }
 
     protected Info buildOpenApiInfo() {
