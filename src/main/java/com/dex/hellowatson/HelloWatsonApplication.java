726262cdcf6ca3b47a3d34487455fcdbac654083
Sean Sundberg <seansund@us.ibm.com>
2023-05-05 11:37:34 -0400

    Update OpenApi documentation
    - Add OpenApi config bean and values in application.yaml
    - Rename package to hellowatson
    - Update workflow to set version number in build.gradle
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/dex/hellowatson/HelloWatsonApplication.java
+++ src/main/java/com/dex/hellowatson/HelloWatsonApplication.java
@@ -1,13 +1,13 @@
-package com.dex.hellowo;
+package com.dex.hellowatson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWoApplication {
+public class HelloWatsonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWoApplication.class, args);
+		SpringApplication.run(HelloWatsonApplication.class, args);
 	}
 
 }
