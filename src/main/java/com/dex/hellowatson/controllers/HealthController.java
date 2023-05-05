34af5cb0296057e4ef7fd9ed25468f72ba56114e
Sean Sundberg <seansund@us.ibm.com>
2023-05-05 13:28:21 -0500

    Hide health endpoint from openapi docs (#12)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/dex/hellowatson/controllers/HealthController.java
+++ src/main/java/com/dex/hellowatson/controllers/HealthController.java
@@ -1,12 +1,14 @@
 package com.dex.hellowatson.controllers;
 
 import com.dex.hellowatson.models.SimpleResponse;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @CrossOrigin()
 public class HealthController {
